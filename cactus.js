f4b01019a0e1fee93ee905a9e622d532e04effcb
cactus1234aaa <100249350+cactus1234aaa@users.noreply.github.com>
2022-11-17 18:07:42 -0500

    Update cactus.js

--- cactus.js
+++ cactus.js
@@ -1,11 +1,15 @@
   // cactusjs
-  function debug(str) {
+console.log(
+  "%cCactus.js is running!",
+  "color:#008b8b;"
+  )
+function debug(str) {
     console.log(str)
  }
   function changeElementTxt(id, str) {
-    document.getElementById(id).innerText = str
+    document.getElementById(id).te = str
   }
-  function winopen(str) {
+  function WinOpen(str) {
     window.open(str)
  }
   function changeElementHtml(id, str) {
@@ -17,7 +21,28 @@
   function print(str) {
     document.write(str)
   }
-  console.log(
-    "%cCactus.js is running!",
-    "color:#008b8b;"
-    )
+    const jsml = {
+      create(element, txt, id) {
+          const para = document.createElement(element);
+      para.innerHTML = txt;
+      document.getElementById(id).appendChild(para);
+      },
+      sc(elementb, attr, val, id) {
+          const para = document.createElement(elementb);
+          const att = document.createAttribute(attr);
+          att.value = val;
+           ;
+          para.setAttributeNode(att);
+          document.getElementById(id).appendChild(para);
+      },
+      cattr(elementb, txtb, attr, val, id) {
+          const para = document.createElement(elementb);
+          para.innerHTML = txtb;
+          const att = document.createAttribute(attr);
+          att.value = val;
+           ;
+          para.setAttributeNode(att);
+          document.getElementById(id).appendChild(para);
+      }
+      
+  };
